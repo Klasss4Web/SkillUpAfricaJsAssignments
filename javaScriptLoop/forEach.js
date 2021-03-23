ef0a29236d279c308040a56083a0c4b091fc4d76
OCHADE EMMANUEL <eochade16@gmail.com>
2021-03-23 11:06:39 +0100

    added more propertirs to the objects nested in the myJsTutor Array

--- javaScriptLoop/forEach.js
+++ javaScriptLoop/forEach.js
@@ -1,19 +1,26 @@
+
 const myJsTutor = [
   {
     name: 'Ebenezer Don',
     goodTutor: true,
-    stack: 'JavaScript Python RubyAndRails',
+    profession: ['Developer', 'Tutor', 'Blogger'],
+    stack: ['JavaScript', 'Python', 'RubyAndRails'],
+    proficiency: 'Master',
+    sloganOne: 'Mastery only comes from building',
+    sloganTwo: 'The goal is to understand and not to cover large portion without understanding'
   },
   {
-    youtubeCourses: 'learn git and gitHub in 20 minutes',
+    youtubeCourses: ['learn git and gitHub in 20 minutes','Chillop in the rain','Simple fast web hosting with gitHub pages'],
     goodTutor: true,
     udemyCourse: 'Web Development for absolute beginners',
-    price: '$11.99' 
+    priceForUdemyCourse: '$11.99',
+    priceForYoutubeCourse: 'freeSubscription'  
   }
 ]
 
-console.log(myJsTutor);
 
-myJsTutor.forEach(item => {
-  console.log(item.goodTutor)
+console.log(myJsTutor)
+
+myJsTutor.forEach(property => {
+  console.log(property.goodTutor)
 })
